6c55b69bb6fa03e648cf91c05d7eebf9498fc349
Amaranth01 <vanessa.nectoux@gmail.com>
2021-11-10 11:58:25 +0100

    Exo 51 ok

---
+++ js-51.js
@@ -0,0 +1,17 @@
+let nb1 = 5;
+let nb2 = 6;
+
+result = nb1 + nb2;
+
+let addition = result + 2;
+
+let soustraction = result - 1;
+
+let multiple = result * addition;
+
+alert (multiple);
+
+alert (result ** soustraction)
+
+let modulo = 5 % 2;
+console.log(modulo)
